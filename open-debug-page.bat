d9b6245485c21e82253b61d725c4c7f923e92e85
chad-exc <chad@elementxcreative.com>
2025-06-09 19:57:05 -0500

    removing old files

--- open-debug-page.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Opening Firefox debugging page...
-start firefox "about:debugging#/runtime/this-firefox"
-echo.
-echo Now click "Load Temporary Add-on..." and select manifest.json
-pause
--- run-extension.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Firefox with Word Counter extension and your normal extensions...
-cd /d "e:\docs\text documents\batch\word_count_firefox"
-web-ext run --firefox-profile=default
-pause
